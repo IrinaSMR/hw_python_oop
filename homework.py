e939d6bb170ccdd874a22ffc8f2cdcc26432ca5f
Ирина Смирнова <ismirnova83@yandex.ru>
2022-03-30 18:10:49 +0300

    Устранено 4 ошибки, 1 осталась

--- homework.py
+++ homework.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Type, Optional
 
 
 class InfoMessage:
@@ -58,7 +58,7 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        info_message = (InfoMessage(self.get_traning_type(), self.duration,
+        info_message = (InfoMessage(self.__class__.__name__, self.duration,
                         self.get_distance(), self.get_mean_speed(),
                         self.get_spent_calories()))
         return info_message
@@ -75,14 +75,12 @@ class Running(Training):
                           * (self.duration * 60))
         return spent_calories
 
-    def get_traning_type(self) -> str:
-        return 'Running'
-
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
     K_1 = 0.035
     K_2 = 0.029
+    MIN_IN_HOUR = 60
 
     def __init__(self, action: int,
                  duration: float,
@@ -95,11 +93,8 @@ class SportsWalking(Training):
         spent_calories = ((self.K_1 * self.weight
                           + (self.get_mean_speed()**2 // self.height)
                           * self.K_2 * self.weight)
-                          * (self.duration * 60))
+                          * (self.duration * self.MIN_IN_HOUR))
         return spent_calories
-
-    def get_traning_type(self) -> str:
-        return 'SportsWalking'
 
 
 class Swimming(Training):
@@ -127,23 +122,19 @@ class Swimming(Training):
                           * 2 * self.weight)
         return spent_calories
 
-    def get_traning_type(self) -> str:
-        return 'Swimming'
-
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
 
-    package: Dict[str, Training] = {'SWM': Swimming,
-                                    'RUN': Running,
-                                    'WLK': SportsWalking
-                                    }
-    if workout_type not in package.keys():
-        return 'Не определено'
+    package: Dict[str, Type[Training]] = {'SWM': Swimming,
+                                          'RUN': Running,
+                                          'WLK': SportsWalking
+                                          }
+    if workout_type not in package:
+        raise NotImplementedError('Неожидаемый тип {workout_type}')
 
-    training_class = package.get(workout_type)
-    training_object = training_class(*data)
-    return training_object
+    training_class: Optional[Type[Training]] = package.get(workout_type)
+    return training_class(*data)
 
 
 def main(training: Training) -> None:
